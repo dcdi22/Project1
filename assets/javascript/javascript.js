c4a86c335ad5c0c0e30537ee553b877304313be0
Asteria <dcdion22@gmail.com>
2018-07-04 22:08:45 -0400

    Working Vimeo Calls

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,10 @@
-
 var genres = [];
 var moods = [];
 var randomTracks = [];
 var spotify_access_token;
 var client_id = 'cf8c0eb349a54b93b25dfb1eaabbf17b'
 var client_secret = '4702fa19b63940e7aefb634fa83392de';
+
 function generateSpotifyAccessToken(cb) {
     $.ajax({
         url: 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token',
@@ -21,6 +21,10 @@ function generateSpotifyAccessToken(cb) {
     }).catch(err => console.error(err));
 }
 
+
+//         //
+// Spotify //
+//         //
 function getArtist(artist, cb) {
     $.ajax({
         method: 'GET',
@@ -36,13 +40,25 @@ function getArtist(artist, cb) {
 }
 
 $(document.body).on("click", '#submit', function () {
+    //event.preventDefault();
     var artist = $("#artistName").val().trim();
-    var vimeoAccessToken = '275bb5cff8e3ae3639a860dd4c0976cf'
-    var vimeoQueryURL = `https://api.vimeo.com/videos?query=${artist}&access_token=${vimeoAccessToken}&per_page=1`;
+    capitalize();
 
-    getArtist(artist, function(tracks) {
+    //                       //
+    // Spotify Function Call //
+    //                       //
+    getArtist(artist, function (tracks) {
         console.log(tracks);
+        console.log(tracks.tracks.href);
     });
+
+
+    //       //
+    // Vimeo //
+    //       //
+    var vimeoAccessToken = '275bb5cff8e3ae3639a860dd4c0976cf'
+    var vimeoQueryURL = `https://api.vimeo.com/videos?query="${artist}"&access_token=${vimeoAccessToken}&per_page=1`;
+    // Get rid of this if using catagories &page=1&per_page=15
 
     $.ajax({
         url: vimeoQueryURL,
@@ -50,11 +66,29 @@ $(document.body).on("click", '#submit', function () {
     }).then(function (response) {
         console.log(response);
 
-        //event.preventDefault();
-        // console.log(response);
+        // var video = response.data.filter(e => e.categories.some(x => x.name === 'Music'))[0];
+
+        // $('#backVid').attr('src', 'https://player.vimeo.com/video/' + video.uri.match(/\d+/) + '?autoplay=1&loop=1&muted=1#t=40s');
+
+        console.log(response.data.filter(e => e.categories.some(x => x.name === 'Music')));
+        console.log(response.data.filter(e => e.categories.some(x => x.name === 'Music'))[0]);
 
         var vimeoResults = response.data;
+        console.log(vimeoResults[0].uri.match(/\d+/)[0]);
+        console.log($("#backVid"));
+        $("#backVid").attr("src", 'https://player.vimeo.com/video/' + vimeoResults[0].uri.match(/\d+/)[0] + '?autoplay=1&loop=1&muted=1#t=40s');
+
+        // var video = response.data.filter(video => video.tags.some(tag => /music/i.test(tag.name)))[0];
+
+        // // console.log(video.uri.match(/\d+/));
+        // // console.log($("#backVid"));
+
+        // $('#backVid').attr('src', 'https://player.vimeo.com/video/' + video.uri.match(/\d+/) + '?autoplay=1&loop=1&muted=1#t=40s')
     })
+
+    //         //
+    // Last.Fm //
+    //         //
     var lastfmAPIKey = "628ffbed5ecc1259d740e1b19182a0bb";
     var lastfmQueryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=" + lastfmAPIKey + "&format=json"
 
@@ -68,4 +102,17 @@ $(document.body).on("click", '#submit', function () {
             $("#artistInfo").html(lastfmResults.bio.summary);
         });
 
+
+    
+
 })
+
+function capitalize() {
+    var artistName = $('#artistName').val();
+    var spart = artistName.split(' ');
+    for (var i = 0; i < spart.length; i++) {
+        var j = spart[i].charAt(0).toUpperCase();
+        spart[i] = j + spart[i].substr(1);
+    }
+    $("#musicArtistName").html(spart.join(' ') + ' ' + '<i class="far fa-play-circle" id="play"></i>');
+}
