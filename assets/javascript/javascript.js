9603a18258056aef1c8cdc8aa29656e6fd1686c2
kabrow3 <kabrow3@gmail.com>
2018-07-07 10:02:41 -0400

    tweak

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -115,14 +115,6 @@ $(document.body).on("click", '#submit', function (event) {
 
     }
     getArtist(artist, playMusic);
-   
-    //       //
-    // Vimeo //
-    //       //
-    var vimeoAccessToken = '275bb5cff8e3ae3639a860dd4c0976cf'
-    var vimeoQueryURL = `https://api.vimeo.com/videos?query="${artist}"&access_token=${vimeoAccessToken}&per_page=1`;
-    // Get rid of this if using catagories &page=1&per_page=15
-
 
     //       //
     // Vimeo //
