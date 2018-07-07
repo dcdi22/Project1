2f33031b8c5b899764f77fae1d5e48eacc417652
kabrow3 <kabrow3@gmail.com>
2018-07-07 02:33:10 -0400

    Pause Btn and Track Names

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,6 +4,34 @@ var randomTracks = [];
 var spotify_access_token;
 var client_id = 'cf8c0eb349a54b93b25dfb1eaabbf17b'
 var client_secret = '4702fa19b63940e7aefb634fa83392de';
+var audioElement = document.createElement("audio");
+
+
+function capitalize() {
+    var artistName = $('#artistName').val();
+    var spart = artistName.split(' ');
+    for (var i = 0; i < spart.length; i++) {
+        var j = spart[i].charAt(0).toUpperCase();
+        spart[i] = j + spart[i].substr(1);
+    }
+    $("#musicArtistName").html(spart.join(' ') + ' ' + '<i class="far fa-pause-circle" id="play"></i>');
+}
+
+function playPause() {
+    if (audioElement.paused) {
+        $("#play").html("<i class='far fa-pause-circle'></i>");
+        audioElement.play();
+    } else if (audioElement.play) {
+        $("#play").html("<i class='far fa-play-circle'></i>");
+        audioElement.pause();
+    }
+}
+
+$(document.body).on("click", '#play', function (event) {
+    event.preventDefault;
+    playPause();
+})
+
 
 function generateSpotifyAccessToken(cb) {
     $.ajax({
@@ -26,6 +54,7 @@ function generateSpotifyAccessToken(cb) {
 // Spotify //
 //         //
 function getArtist(artist, cb) {
+    console.log(cb);
     $.ajax({
         method: 'GET',
         url: 'https://api.spotify.com/v1/search',
@@ -39,10 +68,15 @@ function getArtist(artist, cb) {
     }).then(cb).catch(() => generateSpotifyAccessToken(() => getArtist(artist, cb)));
 }
 
+
 $(document.body).on("click", '#submit', function (event) {
     event.preventDefault();
     var artist = $("#artistName").val().trim();
     capitalize();
+    var playList = [];
+    var trackList = [];
+    var playList_Index = 0;
+    
    
  
     //                       //
@@ -50,29 +84,31 @@ $(document.body).on("click", '#submit', function (event) {
     //                       //
     function playMusic(tracks) {
         console.log(tracks);
-        console.log(tracks.tracks.items[0].preview_url);
-        var playList = [];
-        var playList_Index = 0;
-        var audioElement = document.createElement("audio");
+        
+        
         
         for (var i=0; i < tracks.tracks.items.length; i++) {
             playList.push(tracks.tracks.items[i].preview_url);
+            trackList.push(tracks.tracks.items[i].name);
         }
         console.log(playList);
 
 
-        //  while (playList_Index < playList.length) {
+        //  
             audioElement.setAttribute('src', playList[playList_Index]);
-            // audioElement.setAttribute('id', "track-" + playList_Index);
-            audioElement.play();
-
-            if (playList_Index < playList.length) {
-                audioElement.load();
+            
                 audioElement.play();
-                audioElement.addEventListener('ended', function() {
-                    playList_Index++;
-                    playMusic(tracks);
-                })
+      
+            if (playList_Index < playList.length) {
+               
+                    audioElement.load();
+                    audioElement.play();
+                    $(".track-info").html("Now Playing: " + trackList[playList_Index]);
+                    audioElement.addEventListener('ended', function() {
+                        playList_Index++;
+                        playMusic(tracks);
+                    })
+                
             }
 
             
@@ -100,18 +136,17 @@ $(document.body).on("click", '#submit', function (event) {
         console.log(vimeoResults[0].uri.match(/\d+/)[0]);
         console.log($("#backVid"));
         $("#backVid").attr("src", 'https://player.vimeo.com/video/' + vimeoResults[0].uri.match(/\d+/)[0] + '?autoplay=1&loop=1&muted=1#t=40s');
-
-       
     })
 
     //         //
     // Last.Fm //
     //         //
+ 
     var lastfmAPIKey = "628ffbed5ecc1259d740e1b19182a0bb";
-    var lastfmQueryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=" + lastfmAPIKey + "&format=json"
+    var lastfmQueryURL1 = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=" + lastfmAPIKey + "&format=json"
 
     $.ajax({
-            url: lastfmQueryURL,
+            url: lastfmQueryURL1,
             method: "GET"
         })
         .then(function (response) {
@@ -120,17 +155,25 @@ $(document.body).on("click", '#submit', function (event) {
             $("#artistInfo").html(lastfmResults.bio.summary);
         });
 
+    var lastfmQueryURL2 = "http://ws.audioscrobbler.com/2.0/?method=user.getartisttracks&user=rj&artist=" + artist + "&api_key=" + lastfmAPIKey + "&format=json"
 
-    
+    $.ajax({
+        url: lastfmQueryURL2,
+        method: "GET"
+    })
+    .then(function (response) {
+        var lastfmResults = response.artist;
+        console.log(response);
+        // $("#artistInfo").html(lastfmResults.bio.summary);
+    });
+
+
+
+
+
+
+
+
 
 })
 
-function capitalize() {
-    var artistName = $('#artistName').val();
-    var spart = artistName.split(' ');
-    for (var i = 0; i < spart.length; i++) {
-        var j = spart[i].charAt(0).toUpperCase();
-        spart[i] = j + spart[i].substr(1);
-    }
-    $("#musicArtistName").html(spart.join(' ') + ' ' + '<i class="far fa-play-circle" id="play"></i>');
-}
